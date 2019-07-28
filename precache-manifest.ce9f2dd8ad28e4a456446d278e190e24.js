abc8d7917c8aa136f45fc2a0f57e1294d4a279ad
tonhwk <andres_btk@yahoo.com>
2019-07-28 10:24:27 -0500

    Updates

---
+++ precache-manifest.ce9f2dd8ad28e4a456446d278e190e24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3644b0b05bbb43a5b07ccf6bbe92ba1a",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "889908a7e75595dd63f9",
+    "url": "/react-monsters/static/css/main.e4538e61.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-monsters/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "889908a7e75595dd63f9",
+    "url": "/react-monsters/static/js/main.fa7ecec4.chunk.js"
+  },
+  {
+    "revision": "1c8ad7999933b0847d83",
+    "url": "/react-monsters/static/js/runtime~main.511ef2b8.js"
+  }
+]);
